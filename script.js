bf58c1bef4eb524bf8b0e6b7c3df23fd9ffaf5a8
Iqbal Abdul Ra'uf <iqbalar@student.telkomuniversity.ac.id>
2023-11-28 10:56:24 +0700

    EUREKA! Akhirnya ketemu algoritma tepat

--- script.js
+++ script.js
@@ -1,67 +1,114 @@
-function indoumandana(kalimat) {
+function pisahSukuKataKalimat(kalimat) {
     var kataArray = kalimat.split(" ");
-    var hasilPecah = [];
+    var hasilPemisahan = [];
+
     kataArray.forEach(function (kata) {
-        var sukuKata = [];
-        for (var i = 0; i < kata.length; i += 2) {
-            var duaHuruf = kata.substr(i, 2);
-            if (cekHurufKonsonanDanVokal(duaHuruf)) {
-                sukuKata.push(duaHuruf);
-            } else {
-                sukuKata.push(duaHuruf[0] + duaHuruf[1]);
-            }
-        }
-        
-        for (var j = 0; j < sukuKata.length; j++) {
-            if (sukuKata[j].endsWith("a")) {
-                sukuKata[j] += "iden";
-            } else if  (sukuKata[j].endsWith("i")) {
-                sukuKata[j] += "pri";
-            } else if  (sukuKata[j].endsWith("u")) {
-                sukuKata[j] += "pru";
-            } else if  (sukuKata[j].endsWith("e")) {
-                sukuKata[j] += "pre";
-            } else if  (sukuKata[j].endsWith("o")) {
-                sukuKata[j] += "pro";
-            } else if (sukuKata[j].endsWith("ng")) {
-                sukuKata[j] = "strengen";
-            } else if (['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z'].some(char => sukuKata[j].endsWith(char))) {
-                sukuKata[j] += "es";
-            } else {
-                sukuKata[j];
-            }
-        }
-        var kataHasil = sukuKata.join(" ");
-        hasilPecah.push(kataHasil);
+        var sukuKata = pisahSukuKata(kata);
+        hasilPemisahan.push(sukuKata.join(" "));
     });
 
-    var kalimatHasil = hasilPecah.join(" ");
-    return kalimatHasil;
+    return hasilPemisahan.join(" ");
 }
 
-function cekHurufKonsonanDanVokal(duaHuruf) {
-    var konsonan = "bcdfhjklmnpqrstvwxyz";
-    var vokal = "aeiou";
-    var cekNg = "ng";
+function pisahSukuKata(kata) {
+    var sukuKata = [];
+    var i = 0;
 
-    var hurufPertama = duaHuruf[0];
-    var hurufKedua = duaHuruf[1];
-    var hurufNg = duaHuruf[0] + duaHuruf[1];
+    while (i < kata.length) {
+        var hurufPertama = kata[i];
+        var hurufKedua = kata[i + 1];
 
-    var isHurufPertamaKonsonan = konsonan.includes(hurufPertama);
-    var isHurufPertamaVokal = vokal.includes(hurufPertama);
-    var isHurufKeduaVokal = vokal.includes(hurufKedua);
-    var isHurufNg = cekNg.includes(hurufNg);
-    
-    if(isHurufKeduaVokal == true || isHurufNg == true){
-        return isHurufPertamaKonsonan && isHurufKeduaVokal;
-    } else if (isHurufPertamaVokal == true){
-        return isHurufPertamaVokal;  
-    } else if (isHurufPertamaKonsonan == true && isHurufKeduaVokal == false){
-        return isHurufPertamaKonsonan;  
-    } else {
-        return isHurufPertamaKonsonan;
+        if (isKonsonan(hurufPertama) && isVokal(hurufKedua)) {
+            sukuKata.push(hurufPertama + hurufKedua);
+            i += 2;
+        } else if (isKonsonan(hurufPertama) && isKonsonan(hurufKedua)) {
+            if (hurufPertama + hurufKedua === 'ng') {
+                sukuKata.push('ng');
+                i += 2;
+            } else {
+                sukuKata.push(hurufPertama);
+                i++;
+            }
+        } else {
+            sukuKata.push(hurufPertama);
+            i++;
+        }
     }
+
+    return sukuKata;
+}
+
+function isKonsonan(huruf) {
+    var konsonan = 'bcdfghjklmnpqrstvwxyz';
+    return konsonan.includes(huruf.toLowerCase());
+}
+
+function isVokal(huruf) {
+    var vokal = 'aeiou';
+    return vokal.includes(huruf.toLowerCase());
+}
+
+function tambahkanKata(kata) {
+    var sukuKataAkhir = kata.slice(-1);
+
+    switch (sukuKataAkhir) {
+                    case 'a':
+                        return kata + 'iden';
+                    case 'i':
+                        return kata + 'pri';
+                    case 'e':
+                        return kata + 'pre';
+                    case 'u':
+                        return kata + 'pru';
+                    case 'o':
+                        return kata + 'pro';
+                    case 'g':
+                        if (kata.slice(-2) === 'ng') {
+                            return 'strengen';
+                        }
+                        break;
+                    case 'b':
+                    case 'c':
+                    case 'd':
+                    case 'f':
+                    case 'h':
+                    case 'j':
+                    case 'k':
+                    case 'l':
+                    case 'm':
+                    case 'n':
+                    case 'p':
+                    case 'q':
+                    case 'r':
+                    case 's':
+                    case 't':
+                    case 'v':
+                    case 'w':
+                    case 'x':
+                    case 'y':
+                    case 'z':
+                        return kata + 'es';
+                    default:
+                        return kata;
+                }
+    return kata;
+}
+
+function indoumandana(kalimat) {
+    var kataArray = kalimat.split(" ");
+    var hasilModifikasi = [];
+
+    kataArray.forEach(function (kata) {
+        var sukuKata = pisahSukuKata(kata);
+        var sukuKataModifikasi = sukuKata.map(function (suku) {
+            return tambahkanKata(suku);
+        });
+
+        hasilModifikasi.push(sukuKataModifikasi.join(" "));
+    });
+
+    var kalimatHasil = hasilModifikasi.join(" ");
+    return kalimatHasil;
 }
 
 function umandanaindo(kalimatInput) {
